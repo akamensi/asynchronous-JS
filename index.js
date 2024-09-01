e087b28001969537eae1dacd908549878fb5e0be
akamensi <akez.muhammed@gmail.com>
2024-09-02 00:51:54 +0200

    Building Promises

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+/* fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed:${data}`);
 
   superagent
@@ -18,3 +18,38 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
       console.log(err.message);
     });
 });
+ */
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 😢');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file 😢');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed:${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => console.log('Rondom dog image saved to file!'))
+
+  .catch((err) => {
+    console.log(err.message);
+  });
